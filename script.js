45fa5542775a4b16013c48cfea530272e0a10c03
Johan mlg Karlsson <johan@piratprylar.se>
2023-03-17 21:56:54 +0100

    foo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([59.3, 18], 15);
+var map = L.map('map').setView([59.3, 18], 15); 
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
